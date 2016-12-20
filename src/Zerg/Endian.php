5df16515dd71c20a8427b6ce70f3b9edfa36b8f1
Arhitector <lovesosabitch@yandex.ru>
2016-12-21 01:31:22 +0300

    на основе пакета php-bio

---
+++ src/Zerg/Endian.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Zerg;
+
+/**
+ * Class Endian
+ *
+ * @package Zerg
+ */
+class Endian
+{
+    const ENDIAN_LITTLE  = 1;
+    const ENDIAN_BIG     = 2;
+
+    /**
+     * @var int Is machine order Big or Little endian.
+     */
+    private static $machineEndian;
+
+    /**
+     * @return int
+     */
+    public static function getMachineEndian()
+    {
+        if (!self::$machineEndian) {
+            $testInt = 0x00FF;
+            $p = pack('S', $testInt);
+            self::$machineEndian = $testInt === current(unpack('v', $p)) ? self::ENDIAN_LITTLE : self::ENDIAN_BIG;
+        }
+
+        return self::$machineEndian;
+    }
+
+} 

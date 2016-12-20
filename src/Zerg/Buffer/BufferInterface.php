b32298e65c81f3cc5c6d3f6139dcce3174983603
Arhitector <lovesosabitch@yandex.ru>
2016-12-21 02:14:35 +0300

    BufferInterface обновлён дизайн

--- src/Zerg/Buffer/BufferInterface.php
+++ src/Zerg/Buffer/BufferInterface.php
@@ -22,8 +22,47 @@ interface BufferInterface
 	 *
 	 * @param int $position
 	 *
-	 * @return $this
+	 * @return BufferInterface
 	 */
 	public function setPosition($position);
+	
+	/**
+	 * Gets the eof position.
+	 *
+	 * @return int
+	 */
+	public function getSize();
+	
+	/**
+	 * @param int $bytesCount
+	 *
+	 * @return string
+	 */
+	public function read($bytesCount);
+	
+	/**
+	 * @param string $bytes
+	 *
+	 * @return BufferInterface
+	 */
+	public function write($bytes);
+	
+	/**
+	 * @param int  $bytes
+	 * @param bool $signed
+	 * @param int  $endian
+	 *
+	 * @return int
+	 */
+	public function readInt($bytes = 1, $signed = false, $endian = null);
+	
+	/**
+	 * @param int $int
+	 * @param int $bytes
+	 * @param int $endian
+	 *
+	 * @return BufferInterface
+	 */
+	public function writeInt($int, $bytes = 1, $endian = null);
 	
 }

f17e204f4cddf2a2c846fd0853af2a3d899f58db
Arhitector <lovesosabitch@yandex.ru>
2016-12-21 02:03:53 +0300

    FileStream реализован StreamInterface

---
+++ src/Zerg/FileStream.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zerg;
+
+use RuntimeException;
+use Zerg\Buffer\BitBuffer;
+
+/**
+ * FileStream wraps file and allow fields to read data from it.
+ *
+ * @since 0.1
+ * @package Zerg\Stream
+ */
+class FileStream implements StreamInterface
+{
+    use StreamTrait;
+	
+	/**
+	 * Return new file stream that will read data form given file.
+	 *
+	 * @param string $path Path of file.
+	 *
+	 * @throws \RuntimeException
+	 */
+    public function __construct($path)
+    {
+        if ( ! is_file($path))
+        {
+            throw new RuntimeException('File path not found.');
+        }
+	    
+    	$handle = fopen($path, 'rb');
+        $this->buffer = new BitBuffer($handle);
+    }
+	
+}

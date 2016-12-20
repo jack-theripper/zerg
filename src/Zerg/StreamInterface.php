53414a313dd80ab7dcf831f860d0bc7c74145aed
Arhitector <lovesosabitch@yandex.ru>
2016-12-21 01:52:09 +0300

    Реализован StreamInterface

--- src/Zerg/StreamInterface.php
+++ src/Zerg/StreamInterface.php
@@ -2,6 +2,8 @@
 
 namespace Zerg;
 
+use Zerg\Buffer\BufferInterface;
+
 /**
  * Interface StreamInterface
  *
@@ -9,5 +11,19 @@ namespace Zerg;
  */
 interface StreamInterface
 {
+	
+	/**
+	 * Getter for $buffer property.
+	 *
+	 * @return BufferInterface Object that reads data from file|memory stream.
+	 */
+	public function getBuffer();
+	
+	/**
+	 * Move internal pointer by given amount of bits ahead without reading dta.
+	 *
+	 * @param int $size Amount of bits to be skipped.
+	 */
+	public function skip($size);
 	
 }
